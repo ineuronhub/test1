02415ec6b798b8bb8f9ffeebeea776304ea98d14
ineuronhub <jagrutiwagh2320@gmail.com>
2022-08-02 16:53:20 +0530

    This is my first file push

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mongo:database@cluster0.ktfqv3a.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d={
+    "name" : "jagruti",
+    "surnamw" : "wagh",
+    "email" : "jagrutiwagh2320@gmail.com"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
